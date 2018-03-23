25054a63b32ca6cc9563d65e9820f9cb9daa296c
jawhnypoh <po.johnny@gmail.com>
2018-03-22 21:41:32 -0700

    Added button that opens dialog to filter

--- app/src/main/java/com/example/redditimages/redditgram/MainActivity.java
+++ app/src/main/java/com/example/redditimages/redditgram/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.redditimages.redditgram;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -30,6 +34,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private TextView mLoadingErrorMessageTV;
     private TextView mOverlayTV;
 
+    private Button mFilterButton;
+
     private int timeoutMillis = 2000;
     private long startTimeMillis = 0;
 
@@ -48,17 +54,40 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mFeedListItemsRV = (RecyclerView)findViewById(R.id.rv_feed_list);
         mOverlayTV = (TextView)findViewById(R.id.tv_overlay);
         mOverlayTV.setVisibility(View.VISIBLE);
+        mFilterButton = (Button)findViewById(R.id.filter_button);
+        mFilterButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                buildAlertDialog();
+            }
+        });
 
         // Set up Recycler view for the main activity feed
         mFeedListAdapter = new FeedListAdapter();
         mFeedListItemsRV.setAdapter(mFeedListAdapter);
         mFeedListItemsRV.setLayoutManager(new LinearLayoutManager(this));
         mFeedListItemsRV.setHasFixedSize(true);
-        
+
         // Load The Feed
         loadFeed(true);
 
         getSupportLoaderManager().initLoader(FEED_LOADER_ID, null, this);
+    }
+
+    public void buildAlertDialog() {
+        final CharSequence[] items = {"New", "Top"};
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+            alertDialogBuilder.setTitle("Filter by");
+            alertDialogBuilder.setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // User clicked on item button
+                    Log.d(TAG, "user clicked on  " + items[i].toString());
+                    mFilterButton.setText(items[i].toString());
+                }
+            });
+
+            alertDialogBuilder.show();
     }
 
 
